438d991d897f3d9c2a31d69f2542ef94ae213424
sOUTHeR <southeryf@live.com>
2017-05-22 21:19:52 +0800

    modify

--- ContosoApp/UserControls/AuthenticationControl.xaml.cs
+++ ContosoApp/UserControls/AuthenticationControl.xaml.cs
@@ -21,7 +21,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 //  ---------------------------------------------------------------------------------
-
+using System;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -36,6 +36,11 @@ namespace ContosoApp.UserControls
         {
             InitializeComponent();
             DataContext = ViewModel;
+        }
+
+        private async void  HyperlinkButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+              await Windows.System.Launcher.LaunchUriAsync(new Uri(((HyperlinkButton)sender).Tag.ToString()));
         }
     }
 }
